c7aae6790a30a7054d5eb167d2beada1e4de5385
Jaroslav Rakhmatoullin <jazzoslav@gmail.com>
2020-06-06 08:38:15 +0200

    avoid nestet branches

--- smux.go
+++ smux.go
@@ -17,6 +17,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"crypto"
 	"errors"
 	"fmt"
@@ -27,6 +28,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 	"syscall"
 
 	"golang.org/x/crypto/ssh"
@@ -34,71 +36,104 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+var outWriter = NewMultiWriter()
+var errWriter = NewMultiWriter()
+
+func appendErr (e error) {
+	if e != nil {
+		_, ouch := errWriter.WriteString(fmt.Sprintf("%s\n", e))
+		if ouch != nil {
+			panic(ouch)
+		}
+	}
+}
+
+
 // 1. read standard input
 // 2. start application
 // 3. feed input to application
 func main() {
 
-	var err error
-	var pass []byte
-
-	if pass, err = readPass(); err != nil {
+	pass, err := readPass()
+	if err != nil {
 		log.Fatalln(err)
 	}
 	for _, cmds := range os.Args[1:] {
 		cmd := strings.Split(cmds, " ")
 		comm, args := cmd[0], cmd[1:]
 		if "ssh-add" == comm {
-			err = passInputAgent(args, pass)
+			appendErr(passInputAgent(args, pass))
+
 		} else {
-			err = passInputGeneric(comm, args, pass)
+			appendErr(passInputGeneric(comm, args, pass))
 		}
-		if err != nil {
-			_, _ = os.Stderr.WriteString(fmt.Sprintf("%s %v => %s\n", comm, args, err))
-		}
+		_,_ = errWriter.WriteString(fmt.Sprintf("done with %s", cmd))
 	}
+
+
+	var ioErr error
+	_, ioErr = os.Stderr.WriteString(string(errWriter.Bytes()))
+	if ioErr != nil { panic(ioErr) }
+
+	// hm.
+	// _, ioErr = os.Stderr.WriteString(string(outWriter.Bytes()))
+	// if ioErr != nil {
+	// 	panic(ioErr)
+	// }
+
 }
 
-func passInputGeneric(command string, rest []string, pass []byte) (err error) {
+func passInputGeneric(command string, rest []string, pass []byte) error {
 	cmd := exec.Command(command, rest...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	var stdin io.WriteCloser
-	if stdin, err = cmd.StdinPipe(); err == nil {
-		if err = cmd.Start(); err == nil {
-			if _, err = stdin.Write(pass); err == nil {
-				return nil
-			}
-		}
+	cmd.Stdout = errWriter
+	cmd.Stderr = outWriter
+	stdin, err := cmd.StdinPipe()
+	if nil == err {
+		err = cmd.Start()
+	}
+	if nil == err {
+		_, err = stdin.Write(pass)
+	}
+	if nil == err {
+		err = stdin.Close()
+	}
+	if nil == err {
+		err = cmd.Wait()
 	}
 	return err
 }
 
 func passInputAgent(keyPaths []string, pass []byte) (err error) {
 	var sock net.Conn
-	if sock, err = net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err != nil {
+	var pk crypto.PrivateKey
+	sock, err = net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+	if err != nil {
 		return err
 	}
 	sshAgent := agent.NewClient(sock)
-	var pk crypto.PrivateKey
-	var errata = ""
+	failures := 0
 	for _, keyPath := range keyPaths {
-		var errK error
-		var bytes []byte
-		if bytes, errK = ioutil.ReadFile(keyPath); errK == nil {
-			if pk, errK = parsePrivateKey(bytes, pass); errK == nil {
-				errK = sshAgent.Add(agent.AddedKey{PrivateKey: pk})
-				if errK == nil {
-					_, _ = os.Stderr.WriteString(fmt.Sprintf("added %s\n", keyPath))
-				}
+		data, err := ioutil.ReadFile(keyPath)
+		if nil == err {
+			pk, err = parsePrivateKey(data, pass)
+		}
+		if nil == err {
+			err = sshAgent.Add(agent.AddedKey{PrivateKey: pk})
+		}
+		if nil == err{
+			_, err = errWriter.WriteString(fmt.Sprintf("Identity added: %s\n", keyPath))
+		}
+		if err != nil {
+			failures++
+			_, ouch := errWriter.WriteString(fmt.Sprintf("%s: %s\n", keyPath, err))
+			if ouch != nil {
+				log.Fatal(ouch)
 			}
 		}
-		if errK != nil {
-			errata += fmt.Sprintf("%s: %s\n", keyPath, errK)
-		}
 	}
-	if errata != "" {
-		return errors.New("\n" + errata)
+
+	if failures > 0 {
+		return fmt.Errorf("failed to add %d keys", failures)
 	}
 	return nil
 }
@@ -106,9 +141,9 @@ func passInputAgent(keyPaths []string, pass []byte) (err error) {
 func readPass() (pass []byte, err error) {
 	var fi, _ = os.Stdin.Stat()
 
-	isChardev := fi.Mode() & os.ModeCharDevice != 0
-	isNamedPipe := fi.Mode() & os.ModeNamedPipe != 0
-	if  ! isChardev || isNamedPipe {
+	isChardev := fi.Mode()&os.ModeCharDevice != 0
+	isNamedPipe := fi.Mode()&os.ModeNamedPipe != 0
+	if ! isChardev || isNamedPipe {
 		sin := bufio.NewReader(os.Stdin)
 		pass, _, err = sin.ReadLine()
 	} else {
@@ -129,4 +164,49 @@ func parsePrivateKey(key []byte, pass []byte) (pk crypto.PrivateKey, err error) 
 		return pk, nil
 	}
 	return nil, errors.New("invalid passphrase or bad key")
+}
+
+type MultiWriter struct {
+	mutex  sync.Mutex
+	writer io.StringWriter
+	buffer *bytes.Buffer
+}
+
+
+func NewMultiWriter() *MultiWriter {
+	w := bytes.NewBuffer(make([]byte, 4096))
+	return &MultiWriter{
+		mutex:  sync.Mutex{},
+		buffer: w,
+		writer: bufio.NewWriter(w),
+	}
+}
+
+func (mw MultiWriter) Read(p []byte) (n int, err error) {
+	if nil != mw.buffer {
+		p = mw.buffer.Bytes()
+		mw.buffer.Truncate(0)
+		return len(p), nil
+	}
+	return 0, errors.New("null buffer")
+}
+
+func (mw MultiWriter) Write(p []byte) (n int, err error) {
+	return mw.buffer.Write(p)
+}
+
+func (mw MultiWriter) WriteString(s string) (n int, err error) {
+	mw.mutex.Lock()
+	n, err = mw.writer.WriteString(s)
+	mw.mutex.Unlock()
+	return n, err
+}
+
+func (mw MultiWriter) Close() error {
+	mw.buffer = nil
+	return nil
+}
+
+func (mw MultiWriter) Bytes() []byte {
+	return mw.buffer.Bytes()
 }
